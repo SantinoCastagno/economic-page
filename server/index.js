c9175442a791645cae53df11b59dedc8a0a6a276
Mariano Conchillo <marianoconchillo@hotmail.com>
2020-11-26 15:16:19 -0300

    script added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.use('/api', require('./controller.js'));
 
 const PORT = process.env.PORT || 3000;
